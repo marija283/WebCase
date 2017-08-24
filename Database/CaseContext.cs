ef63ad89cb7a6558f2f2dbdd306ed1cf0c4c2dc0
Marija Todorova <marija283@hotmail.com>
2017-08-24 15:00:52 +0200

    db

---
+++ Database/CaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using WebCase.Models;
+
+namespace Database
+{
+    public class CaseContext : DbContext
+    {
+        public CaseContext() : base("CaseContext")
+        {
+
+        }
+
+        public DbSet<Case> Contacts { get; set; }
+    }
+}
---
+++ WebCase.Models/Case.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCase.Models
+{
+    class Case
+    {
+        public int ID { get; set; }
+
+        public String caseNumber { get; set; }
+
+        public String kind { get; set; }
+
+        public String customerNumber { get; set; }
+           
+    }
+}
