c86704bc80ec101aed7611f7d8d0bace5fa1a862
Masha Bekjkovikj <Masha.Bekjkovikj@asseco-see.mk>
2017-08-29 14:03:12 +0200

    GetCaseById added

---
+++ TestProject/StepDefinitions/GetCaseById.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TestProject.StepDefinitions
+{
+    [Binding]
+    public class GetCaseById
+    {
+        [Given(@"Started the test")]
+        public void GivenStartedTheTest()
+        {
+         //   ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Get case (.*)")]
+        public void ThenGetCase(string p0)
+        {
+        //    ScenarioContext.Current.Pending();
+        }
+    }
+}
