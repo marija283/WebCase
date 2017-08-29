b9f2af53fb730f0bcc61535b284e73de7987a44e
Marija Todorova <marija283@hotmail.com>
2017-08-29 12:34:56 +0200

    test getCases

---
+++ TestProject/Features/SpecFlowFeature2.feature
@@ -0,0 +1,21 @@
+﻿Feature: CaseGetById
+	
+
+Background:
+	Given the following cases
+		| id | caseNmber | kind | customerNumer | attachment        |
+		| 1  | 123       | nice | 456           | NULL              |
+		| 2  | NULL      | NULL | NULL          | NULL              |
+		| 3  | 1223      | 132  | 555           | attachments/3.pdf |
+
+
+
+
+@mytag
+Scenario: GetCase
+	#Given I have entered 50 into the calculator
+	#And I have entered 70 into the calculator
+	When I search for cases by the 'id'
+	Then the list of found books should be:
+		| id | caseNmber | kind | customerNumer | attachment        |
+		| 1  | 123       | nice | 456           | NULL              |
---
+++ TestProject/Steps/CaseGetByIdSteps.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+using CaseDatabase;
+using WebCase.Services;
+using WebCase.Models;
+using WebCase.Controllers;
+using System.Web.Mvc;
+
+namespace TestProject
+{
+    [Binding]
+    public class CaseGetByIdSteps
+    {
+        private ICaseRepo caseRepo;
+
+        private CaseContext _caseContext;
+
+        private Case myCase;
+
+        [Given(@"the following cases")]
+        public void GivenTheFollowingCases(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I search for cases by the '(.*)'")]
+        public void WhenISearchForCasesByThe(int caseId)
+        {
+            // ScenarioContext.Current.Pending();
+            // var book = _caseContext.GetById(p0));
+            // var controller = new CatalogController();
+            // actionResult = controller.Details(book.Id);
+
+            caseRepo = new CaseRepoImpl();
+            var book2 = caseRepo.GetCases(caseId);
+            var controller = new CasesController();
+            // var cases = controller.Get(caseId);
+
+        }
+
+        [Then(@"the list of found books should be:")]
+        public void ThenTheListOfFoundBooksShouldBe(Table table)
+        {
+            //ScenarioContext.Current.Pending();
+            var caseDetails = myCase;
+
+            var row = table.Rows.Single();
+            Assert.AreEqual(row["caseNumber"], caseDetails.caseNumber, "caseNumber.");
+            Assert.AreEqual(row["kind"], caseDetails.kind, "kind.");
+            Assert.AreEqual(Convert.ToDecimal(row["customerNumber"]), caseDetails.customerNumber, "customerNumber.");
+        }
+    }
+}
